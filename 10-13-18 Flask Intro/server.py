0525a5bb17c4e345092aae5c1be2862dcd5d6c1e
CodeHotPink <42747920+CodeHotPink@users.noreply.github.com>
2018-10-15 13:56:51 -0700

    Add files via upload

---
+++ 10-13-18 Flask Intro/server.py
@@ -0,0 +1,89 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+@app.route("/")
+def start_here():
+    """Home page."""
+    return """<!doctype html><html><a href="/hello">This is link for hello</a><br>Hi! home page.</html>"""
+
+
+@app.route("/hello")
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          <br>
+          Choose a compliment: 
+            <select name="compliment">
+            <option value="cool">Cool</option>
+            <option value="awesome">Awesome</option>
+            </select>
+          <input type="submit" value="Submit">
+          <br>
+        </form>
+        <a href="/diss">I don't want to give a compliment</a>
+      </body>
+    </html>
+    """
+
+
+@app.route("/greet")
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    compliment = request.args.get("compliment")
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {}! I think you're {}!
+      </body>
+    </html>
+    """.format(player, compliment)
+
+@app.route("/diss")
+def give_insult():
+  """just playing around right now"""
+  return """
+  <!doctype html>
+  <html>
+    <head>
+      <title>Insults</title>
+    </head>
+    <body>
+      Too bad.
+    </body>
+  </html>"""
+
+
+if __name__ == "__main__":
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0")
