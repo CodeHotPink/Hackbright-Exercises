0d8052e2f59a7eb7902a8f58c929c69e60e4a132
CodeHotPink <42747920+CodeHotPink@users.noreply.github.com>
2018-10-16 09:54:28 -0700

    transfer into folder

---
+++ 10-09-18 Class 3 Melons/melons.py
@@ -0,0 +1,64 @@
+"""Classes for melon orders."""
+from random import randint
+
+
+class AbstractMelonOrder():
+    """An abstract base class that other Melon Orders inherit from"""
+
+    def __init__(self, species, qty, country_code = None):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.country_code = country_code
+        self.shipped = False
+
+    def get_base_price(self):
+        price = randint(5,9)
+        return price
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = self.get_base_price()
+        if self.species == "christmas":
+            base_price *= 1.5
+        total = (1 + self.tax) * self.qty * base_price
+        if self.order_type == "international" and self.qty < 10:
+            total += 3
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    tax = 0.08
+    order_type = "domestic"
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    tax = 0.17
+    order_type = "international"
+
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """US Government melon order."""
+
+    tax = 0.00
+    passed_inspection = False
+
+    def mark_inspection(self, passed):
+        """Changes passed_inspection if True"""
+        self.passed_inspection = passed
